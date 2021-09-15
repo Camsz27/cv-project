ab09ae32c97234c73111241ff7e6736ff9755941
Camsz27 <camiloagr2707@gmail.com>
2021-09-14 22:08:57 -0500

    Added components of the header of the cv

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Information from './components/Information';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Information></Information>
+    </div>
+  );
 }
 
 export default App;
